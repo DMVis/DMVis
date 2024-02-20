19e267445d56f85143b41338611fbbb9924b5dcc
jelte-akker <jelte2410@gmail.com>
2024-02-20 15:18:47 +0100

    Added d3 with typing and created a base component template

---
+++ src/lib/components/base/Template.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  // Insert imports
+
+  // Insert exports
+
+  // Insert logic
+</script>
+
+<!-- Insert HTML code below -->
+
+<style>
+  /* Insert component styling  */
+</style>
