60059266eba927816309f40160a0fd37d6e9197b
jelte-akker <jelte2410@gmail.com>
2024-02-22 15:28:22 +0100

    Added a basic line component

---
+++ src/lib/components/base/Line.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  // imports
+  import type { ScaleLinear } from 'd3';
+
+  // exports
+  export let points: [{ x: number; y: number }];
+  export let xScale: ScaleLinear<number, number>;
+  export let yScale: ScaleLinear<number, number>;
+
+  // Optional exports
+  export let color: string = '#000';
+  export let width: number = 1;
+
+  // logic
+  $: path = `M${points.map((p) => `${xScale(p.x)},${yScale(p.y)}`).join('L')}`;
+</script>
+
+<path stroke={color} stroke-width={width} class="path" d={path} />
+
+<style>
+  /* Insert component styling  */
+</style>
