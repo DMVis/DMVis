7fa18fc3941f5b1f2eb3f1b73e6f86b2691d90fd
Mstiekema <m.stiekema@students.uu.nl>
2024-02-23 11:42:59 +0100

    Added component documentation to the template

--- src/lib/components/base/Template.svelte
+++ src/lib/components/base/Template.svelte
@@ -6,7 +6,20 @@
   // Insert logic
 </script>
 
-<!-- Insert HTML code below -->
+<!--
+@component
+### Component Name
+A small description of the component.
+
+#### Required attributes
+* attribute: type  - Description.
+
+#### Optional attributes
+* attribute: type  - Description. Default is 'default'.
+-->
+<g>
+  <!-- Insert component markup  -->
+</g>
 
 <style>
   /* Insert component styling  */
