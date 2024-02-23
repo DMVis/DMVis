b3ec92cc37dc9ec1dcf7435f76102ac2ffc2eb8c
Joel <j.g.vandehoeve@students.uu.nl>
2024-02-23 10:35:42 +0000

    Added Point component with documentation

---
+++ src/lib/components/base/Point.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let x: number; //Note that these coordinates need to be scaled beforehand!
+  export let y: number; //Note that these coordinates need to be scaled beforehand!
+  export let r: number = 5;
+  export let c: string = 'rgb(0, 153, 255)';
+  export let borderColor: string = 'black';
+  export let borderWidth: number = 1;
+</script>
+
+<!--
+@component
+### Point
+The point allows you to plot a point at a given coordinate.
+It is used in combination with other components to create a chart.
+
+#### Required attributes
+  * x: number             - Scaled x-coordinate of the point
+  * y: number             - Scaled y-coordinate of the point
+
+#### Optional attributes
+  * r: number             - Radius of the point, defaulted to 5
+  * c: string             - Color of the center of the point, defaulted to blue. Can be any hex-code, rgb or plain string colors
+  * borderColor: string   - Color of the border of the point, defaulted to Black. Can be any hex-code, rgb or plain string colors
+  * borderWidth: number   - Width of the border, defaulted to 1
+-->
+<circle cx={x} cy={y} {r} stroke={borderColor} fill={c} stroke-width={borderWidth} />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
+<script lang="ts">
+  import Point from '$lib/components/base/Point.svelte';
+
+  const numPoints: number = 200;
+  const size: { x: number; y: number } = { x: 400, y: 400 };
+  const data: { x: number; y: number }[] = genPoints();
+
+  function genPoints() {
+    let res: { x: number; y: number }[] = [];
+    for (let i = 0; i < numPoints; i++) {
+      let xRandom: number = Math.floor(Math.random() * size.x);
+      let yRandom: number = Math.floor(Math.random() * size.y);
+      res.push({ x: xRandom, y: yRandom });
+    }
+    return res;
+  }
+</script>
+
 <h1>DMVis</h1>
 <p>Framework for making complex decision matrix visualizations.</p>
+
+<svg style="border: 1px solid black" width="400" height="400">
+  {#each data as p}
+    <Point x={p.x} y={p.y} r={6} />
+  {/each}
+</svg>
