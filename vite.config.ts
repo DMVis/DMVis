551e73e1f8d3a956ab6ffd4575b1103787544cea
Marcel van laar <m.s.laar.v@gmail.com>
2024-03-26 13:50:55 +0100

    Test coverage with vitest/coverage-v8

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,31 @@ export default defineConfig(({ mode }) => ({
     include: ['src/**/*.{test,spec}.{js,ts}'],
     setupFiles: ['./src/vitest/cleanupDom.ts'],
     reporter: 'verbose',
-    restoreMocks: true
+    restoreMocks: true,
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        '**/documentation/**',
+        '**/routes/+page.svelte',
+        '**/svelte.config.js',
+        'coverage/**',
+        'dist/**',
+        '**/[.]**',
+        'packages/*/test?(s)/**',
+        '**/*.d.ts',
+        '**/virtual:*',
+        '**/__x00__*',
+        '**/\x00*',
+        'cypress/**',
+        'test?(s)/**',
+        'test?(-*).?(c|m)[jt]s?(x)',
+        '**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)',
+        '**/__tests__/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        '**/vitest.{workspace,projects}.[jt]s?(on)',
+        '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}'
+      ]
+    }
   },
   server: {
     watch: {
