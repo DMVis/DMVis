d964bf1a78aae9641dd28753d8043fde1926050f
Scharenburg, A.G. van (Anthony) <a.g.vanscharenburg@students.uu.nl>
2024-05-14 11:10:39 +0000

    Force numerical data into categorical when toggled by parameter.

--- documentation/visualisations/Histogram.md
+++ documentation/visualisations/Histogram.md
@@ -8,13 +8,13 @@ This visualisation shows frequencies of data. It can group data categorically or
 
 - Type: `number`
 
-Width of the visualisation.
+The width of the visualisation.
 
 ## height
 
 - Type: `number`
 
-Height of the visualisation.
+The height of the visualisation.
 
 ## data
 
@@ -29,70 +29,89 @@ An array of data which should be shown in the histogram. They will be represente
 - Type: `number`
 - Default: `0`
 
-X position of the histogram.
+The x position of the histogram.
 
-## showOuterTicks
+## y
 
-- Type: `boolean`
-- Default: `true`
+-Type: `number`
 
-Specifies if you want to show only the outer ticks. Otherwise, it will show all ticks.
+- Default: `0`
 
-## bins
-
-- Type: `number`
-- Default: `d3.max(data as number[]) as number`
-
-Specifies the number of bins. This only has an impact on numerical data.
-Categorical data is divided into unique categories (strings).
-Bucketing multiple strings together makes the histogram less useful/clear.
-To make buckets for numerical data we use the d3.bin function which groups numerical values together in buckets with equally large ranges.
-This d3.bin function has a parameter called 'thresholds()' where you can enter a number to specify how many buckets you want. Here we use the bins variable as its input and thus should specify the number of buckets.
-However, the d3.bin().thresholds(bins) do not enforce the precise amount of buckets you wish to have.
-It only tries to get as close as possible to the specified number of 'bins' and may make more or less than the value assigned to 'bins'.
-This is heavily influenced by the data you give it; with bins set to 5 it could make 5 buckets with one set of data, but 7 with another.
+The y position of the histogram.
 
 ## marginLeft
 
 - Type: `number`
 - Default: `40`
 
-Margin to the left of the visualisation.
+The margin to the left of the visualisation.
 
 ## marginRight
 
 - Type: `number`
 - Default: `40`
 
-Margin to the right of the visualisation.
+The margin to the right of the visualisation.
 
 ## marginTop
 
 - Type: `number`
 - Default: `40`
 
-Margin to the top of the visualisation.
+The margin to the top of the visualisation.
 
 ## marginBottom
 
 - Type: `number`
 - Default: `40`
 
-Margin to the bottom of the visualisation.
+The margin to the bottom of the visualisation.
+
+## bins
+
+- Type: `number`
+- Default: `d3.max(data as number[]) as number`
+
+The number of bins of numerical values. This only has an impact on numerical data.
+Categorical data is divided into unique categories (strings).
+Bucketing multiple strings together makes the histogram less useful/clear.
+To make buckets for numerical data we use the d3.bin function which groups numerical values in buckets with equally large ranges.
+This d3.bin function has a parameter called 'thresholds()' where you can enter a number to specify how many buckets you want. Here we use the bins variable as its input and thus should specify the number of buckets.
+However, the d3.bin().thresholds(bins) do not enforce the precise amount of buckets you wish to have.
+It only tries to get as close as possible to the specified number of 'bins' and may make more or less than the value assigned to 'bins'.
+This is heavily influenced by the data you give it; with bins set to 5 it could make 5 buckets with one set of data, but 7 with another.
+
+## showOuterTicks
+
+- Type: `boolean`
+- Default: `true`
+
+A toggle to show only outer ticks.
+When `true`: show only the outer ticks.
+Otherwise: show all ticks.
+
+## forceCategorical
+
+- Type: `boolean`
+- Default: `true`
+
+A toggle to force numerical data into a categorical format.
+When `true`: forces numerical data into a categorical format.
+Otherwise: keep numerical as is.
 
 ## padding
 
 - Type: `number`
-- Default: `0`
+- Default: `0.03`
 
-Value for the distance between each bar in the range [0..1].
+The value for the distance between each bar in the range [0..1].
 
 ## color
 
 - Type: `string`
 - Default: `'blue'`
 
-Colour of each bar.
+The colour of each bar.
 
 ## opacity
 
@@ -107,7 +126,7 @@ a percentage string formatted as '{number}%'.
 - Type: `number | string`
 - Default: `0`
 
-Horizontal corner radius of each bar as a number in the range [0..1] or
+The horizontal corner radius of each bar as a number in the range [0..1] or
 a percentage string formatted as '{number}%'.
 
 ## radiusY
@@ -115,11 +134,17 @@ a percentage string formatted as '{number}%'.
 - Type: `number | string`
 - Default: `0`
 
-Vertical corner radius of each bar as a number in the range [0..1] or
+The vertical corner radius of each bar as a number in the range [0..1] or
 a percentage string formatted as '{number}%'.
 
 # Example usage
 
+The example below shows a histogram with numerical values formated into categories.
+
 ```svelte
-<Histogram width={500} height={500} {data} bins={5} />
+<Histogram
+  width={500}
+  height={500}
+  data={[1, 3, 4, 2, 6, 7, 1, 7, 9, 6, 1]}
+  forceCategorical={true} />
 ```
--- src/lib/components/visualisations/Histogram.svelte
+++ src/lib/components/visualisations/Histogram.svelte
@@ -14,13 +14,18 @@
 
   // Optional attributes
   export let x: number = 0;
-  export let showOuterTicks: boolean = true;
-  export let bins: number = d3.max(data as number[]) as number;
+  export let y: number = 0;
   export let marginLeft: number = 10;
   export let marginRight: number = 10;
-  export let marginBottom: number = 20;
   export let marginTop: number = 20;
-  export let padding: number = 0;
+  export let marginBottom: number = 20;
+
+  // The default number of bins are calculated based on the max value in the data divided by 2
+  export let bins: number = (d3.max(data as Array<number>) ?? 20) / 2;
+  export let showOuterTicks: boolean = true;
+  export let forceCategorical: boolean = true;
+  export let padding: number = 0.03;
+
   export let color: string = 'blue';
   export let opacity: number | string = 1;
   export let radiusX: number | string = 0;
@@ -33,37 +38,76 @@
   let categoricalScale: d3.ScaleBand<string>;
   let numericalScale: d3.ScaleLinear<number, number>;
 
-  // Check if data is categorical or numerical by checking the first entry in the array
+  // Check if data is categorical or numerical by checking the first entry in the 'data' array
+  // Data is categorical
   if (typeof data[0] === 'string') {
-    data.forEach((d) => {
+    data.forEach((category) => {
       // When an element has not yet been processed, add it to buckets with all its duplicates
-      if (typeof categoricalBuckets.find((element) => element.key === d) === 'undefined') {
+      if (typeof categoricalBuckets.find((element) => element.key === category) === 'undefined') {
         categoricalBuckets.push({
-          key: d as string,
-          values: data.filter((element) => element === d) as string[]
+          key: category as string,
+          values: data.filter((element) => element === category) as string[]
         });
       }
-      // Scale from all categorical data
+      // Scale used for placing bars
       categoricalScale = d3
         .scaleBand()
         .domain(categoricalBuckets.map((element) => element.key))
         .range([marginLeft, width - marginRight])
         .padding(padding);
+
+      // Scale used for length the bars
       yScale = d3
         .scaleLinear()
         .domain([0, d3.max(categoricalBuckets.map((element) => element.values.length)) ?? 0])
         .range([height - marginBottom - marginTop, 0]);
     });
+
+    // Data is numerical
   } else {
+    // Group data into buckets representing a range of numbers
     numericalBuckets = d3.bin().thresholds(bins)(data as number[]);
-    numericalScale = d3
-      .scaleLinear()
-      .domain([0, numericalBuckets[numericalBuckets.length - 1].x1 ?? 0])
-      .range([marginLeft, width - marginRight]);
-    yScale = d3
-      .scaleLinear()
-      .domain([0, d3.max(numericalBuckets, (d) => d.length) ?? 0])
-      .range([height - marginBottom - marginTop, 0]);
+
+    // Force the numerical data into categorical data
+    // Make categories using the inclusive lower and exlusive upper bound of each bucket
+    if (forceCategorical) {
+      numericalBuckets.forEach((bucket) =>
+        categoricalBuckets.push({
+          key: bucket.x0 + '-' + (bucket.x1 ?? 0),
+          values: bucket.map((value) => value.toString())
+        })
+      );
+
+      // Scale used for placing bars
+      categoricalScale = d3
+        .scaleBand()
+        .domain(categoricalBuckets.map((element) => element.key))
+        .range([marginLeft, width - marginRight])
+        .padding(padding);
+
+      // Scale used for length the bars
+      yScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(categoricalBuckets.map((element) => element.values.length)) ?? 0])
+        .range([height - marginBottom - marginTop, 0]);
+
+      // Keep numerical as numerical
+    } else {
+      // Scale used for placing bars
+      numericalScale = d3
+        .scaleLinear()
+        .domain([
+          numericalBuckets[0].x0 ?? 0,
+          numericalBuckets[numericalBuckets.length - 1].x1 ?? 0
+        ])
+        .range([marginLeft, width - marginRight]);
+
+      // Scale used for length of the bars
+      yScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(numericalBuckets, (bucket) => bucket.length) ?? 0])
+        .range([height - marginBottom - marginTop, 0]);
+    }
   }
 </script>
 
@@ -73,43 +117,48 @@
 This visualisation shows frequencies of data. It can group data categorically or numerically.
 
 #### Required attributes
-* width: number                - Width of the visualisation.
-* height: number               - Height of the visualisation.
-* data: Array<string | number> - Array of data which should be shown in the histogram. They will be represented as bars.
+* width: number                - The width of the visualisation.
+* height: number               - The height of the visualisation.
+* data: Array<string | number> - An array of data which should be shown in the histogram. They will be represented as bars.
 
 #### Optional attributes
-* x: number                        - X position of the histogram.
-* showOuterTicks: boolean          - Specifies if you want to show only the outer ticks. Otherwise shows all ticks.
-* bins: number                     - Specifies the number of bins. This only has an impact on numerical data.
+* x: number                        - The x position of the histogram. Defaults to `0`.
+* y: number                        - The y position of the histogram. Defaults to `0`.
+* marginLeft: number               - The margin to the left of the visualisation. Defaults to `10`.
+* marginRight: number              - The margin to the right of the visualisation. Defaults to `10`.
+* marginTop: number                - The margin to the top of the visualisation. Defaults to `20`.
+* marginBottom: number             - The margin to the bottom of the visualisation. Defaults to `20`.
+* bins: number                     - The number of bins of numerical values. This only has an impact on numerical data.
                                      Categorical data is divided into unique categories (strings).
                                      Bucketing multiple strings together makes the histogram less useful/clear.
                                      To make buckets for numerical data we use the d3.bin function which groups numerical values in buckets with equally large ranges.
                                      This d3.bin function has a parameter called 'thresholds()' where you can enter a number to specify how many buckets you want. Here we use the bins variable as its input and thus should specify the number of buckets.
                                      However, the d3.bin().thresholds(bins) do not enforce the precise amount of buckets you wish to have.
                                      It only tries to get as close as possible to the specified number of 'bins' and may make more or less than the value assigned to 'bins'.
-                                     This is heavily influenced by the data you give it; with bins set to 5 it could make 5 buckets with one set of data, but 7 with another.
-* marginLeft: number               - Margin to the left of the visualisation.
-* marginRight: number              - Margin to the right of the visualisation.
-* marginTop: number                - Margin to the top of the visualisation.
-* marginBottom: number             - Margin to the bottom of the visualisation.
-* padding: number                  - Value for the distance between each bar in the range [0..1].
-* color: string = 'blue'           - Color of each bar.
-* opacity: number | string         - Opacity of each bar as a number in the range [0..1] or
-                                     a percentage string formatted as '{number}%'.
-* radiusX: number | string         - Horizontal corner radius of each bar as a number in the range [0..1] or
-                                     a percentage string formatted as '{number}%'.
-* radiusY: number | string         - Vertical corner radius of each bar as a number in the range [0..1] or
-                                     a percentage string formatted as '{number}%'.
+                                     This is heavily influenced by the data you give it; with bins set to 5 it could make 5 buckets with one set of data, but 7 with another. Defaults to `(d3.max(data as Array<number>) ?? 20) / 2`.
+* showOuterTicks: boolean          - A toggle to show only outer ticks.
+                                     When `true`: show only the outer ticks.
+                                     Otherwise: show all ticks. Defaults to `true`.
+* forceCategorical: boolean        - A toggle to force numerical data into a categorical format.
+                                     When `true`: forces numerical data into a categorical format.
+                                     Otherwise: keep numerical as is. Deafaults to `true`.
+* padding: number                  - The value for the distance between each bar in the range [0..1]. Defaults to `0.03`.
+* color: string                    - The color of each bar. Defaults to `blue`.
+* opacity: number | string         - The opacity of each bar as a number in the range [0..1] or
+                                     a percentage string formatted as '{number}%'. Defaults to `1`.
+* radiusX: number | string         - The horizontal corner radius of each bar as a number in the range [0..1] or
+                                     a percentage string formatted as '{number}%'. Defaults to `0`.
+* radiusY: number | string         - The vertical corner radius of each bar as a number in the range [0..1] or
+                                     a percentage string formatted as '{number}%'. Defaults to `0`.
 -->
-<svg class="visualisation histogram" {width} {height} {x}>
-  <!-- If the categoricalBuckets is not empty it means the histogram should be rendered as categorical -->
+<svg class="visualisation histogram" {width} {height} {x} {y}>
+  <!-- Draw categorical bars because categoricalBuckets is not empty -->
   {#if categoricalBuckets.length > 0}
     {#each categoricalBuckets as bucket}
-      <!-- Draw Bars -->
       <Bar
         x={categoricalScale(bucket.key) ?? 0}
         y={height - marginBottom}
-        width={categoricalScale.bandwidth() - 1}
+        width={categoricalScale.bandwidth()}
         height={yScale(0) - yScale(bucket.values.length)}
         isVertical={true}
         originX={OriginX.Left}
@@ -119,13 +168,15 @@ This visualisation shows frequencies of data. It can group data categorically or
         {radiusX}
         {radiusY} />
     {/each}
+    <!-- Draw numercial bars -->
   {:else}
     {#each numericalBuckets as bucket}
-      <!-- Draw Bars -->
       <Bar
         x={numericalScale(bucket.x0 ?? 0) + 1 ?? 0}
         y={height - marginBottom}
-        width={numericalScale(bucket.x1 ?? 0) - numericalScale(bucket.x0 ?? 0) - 1}
+        width={numericalScale(bucket.x1 ?? 0) -
+          numericalScale(bucket.x0 ?? 0) -
+          (numericalScale(bucket.x1 ?? 0) - numericalScale(bucket.x0 ?? 0)) * padding}
         height={yScale(0) - yScale(bucket.length)}
         isVertical={true}
         originX={OriginX.Left}
@@ -136,18 +187,19 @@ This visualisation shows frequencies of data. It can group data categorically or
         {radiusY} />
     {/each}
   {/if}
-  <!-- Draw categorical Axis because the data was categorical -->
+  <!-- Draw categorical Axis because categoricalBuckets is not empty -->
   {#if categoricalBuckets.length > 0}
     <Axis
       placementX={0}
       placementY={height - marginBottom}
-      axis={d3.axisBottom(categoricalScale)} />
-    <!-- Draw numerical Axis cause the data was numerical -->
+      axis={d3.axisBottom(categoricalScale).tickSizeOuter(0)} />
+    <!-- Draw numerical Axis with only the outer ticks -->
   {:else if showOuterTicks}
     <Axis
       placementX={0}
       placementY={height - marginBottom}
       axis={d3.axisBottom(numericalScale).tickValues(numericalScale.domain())} />
+    <!-- Draw numerical Axis with all ticks -->
   {:else}
     <Axis placementX={0} placementY={height - marginBottom} axis={d3.axisBottom(numericalScale)} />
   {/if}
