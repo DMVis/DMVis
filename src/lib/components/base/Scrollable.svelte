a6b92867af8bb0ee3eba7b7d98dcbdb2a1a00b43
Jesper-de-Jong <j.dejong19@students.uu.nl>
2024-05-16 11:31:38 +0200

    Added scrollable component

---
+++ src/lib/components/base/Scrollable.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let x: number = 0;
+  export let y: number = 0;
+  export let width: number = 0;
+  export let height: number = 0;
+  export let className: string = '';
+  export let allowHorizontalScrolling: boolean = true;
+  export let allowVerticalScrolling: boolean = true;
+
+  let horizontalScroll: string;
+  $: horizontalScroll = allowHorizontalScrolling ? 'auto' : 'hidden';
+  let verticalScroll: string;
+  $: verticalScroll = allowVerticalScrolling ? 'auto' : 'hidden';
+</script>
+
+<!--
+@component
+### Scrollable
+
+#### Optional Attributes
+
+#### Events
+-->
+<div
+  class="scrollable"
+  id="scrollable-{className}"
+  style={`width:${width}px;
+            height:${height}px;
+            overflow-x:${horizontalScroll};
+            overflow-y:${verticalScroll};`}>
+  <slot />
+</div>
