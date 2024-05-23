1da79a1f2a34df09baa242fc49823da739f4ac53
jelte-akker <jelte2410@gmail.com>
2024-05-23 20:09:19 +0200

    Added missing export to the library

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,6 +5,9 @@ export { default as StackedBarChart } from '$lib/components/visualisations/Stack
 export { default as ScatterplotMatrix } from '$lib/components/visualisations/ScatterplotMatrix.svelte';
 export { default as TabularVisualisation } from '$lib/components/visualisations/TabularVisualisation.svelte';
 export { default as Filter } from '$lib/components/base/Filter.svelte';
+export { default as ValueChart } from '$lib/components/visualisations/ValueChart.svelte';
+export { default as LineUp } from '$lib/components/visualisations/LineUp.svelte';
+export { default as Histogram } from '$lib/components/visualisations/Histogram.svelte';
 
 // Export dataUtils
 export * from '$lib/utils/DataUtils.js';
