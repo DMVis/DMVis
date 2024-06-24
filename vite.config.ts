e789f7dd2431664d3112c85146f2d4ffec6a66ad
Mstiekema <m.stiekema@students.uu.nl>
2024-06-24 14:02:22 +0200

    Added CI for GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,8 @@ export default defineConfig(({ mode }) => ({
         '**/__tests__/**',
         '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
         '**/vitest.{workspace,projects}.[jt]s?(on)',
-        '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}'
+        '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}',
+        '**/lib/*.ts'
       ]
     }
   },
